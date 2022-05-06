22b6e1547688df87f4110a71baab9dd9d500c791
augustojaml <jamonteirolima@gmail.com>
2022-05-06 16:31:12 -0300

    atualizado porta

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log(`#   Server is running in port ${3333} 🖥     #`);
+  console.log(`Server is running in port ${3333} 🖥`);
 });
